e8a1eacb3a96038844be73eb7f647602c95ea82d
JaswaniV <vjaswan78@gmail.com>
2026-01-30 18:33:25 +0530

    Used promises instead of callback

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,8 +5,67 @@ import { loadProducts } from '../data/products.js';
 // import '../data/backend-practice.js'
 
 
+//Promise Practice
+/* 
+new Promise( (resolve) => {
+    console.log('Start Promise');
+    loadProducts( () => {
+        console.log('start Loading');
+        resolve();
+        console.log('finished laoding')
+    });
+}).then( () => { //Using the then function
+    console.log('Next Step')// this is the actual next step resolve will go to
+})
+*/
+
+
+/*Example of promise.all() using loadCart just for example
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+})
+]).then((values) => {
+console.log(values);
+renderOrderSummary();
+renderPaymentSummary();
+});
+
+ */
+
+
+
+new Promise( (resolve) => {
+  loadProducts( () => {
+      resolve(resolve);
+  });
+}).then( (value) => {
+  renderOrderSummary(); 
+  renderPaymentSummary();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 loadProducts( () =>{
     renderOrderSummary(); 
     renderPaymentSummary();
 })
 //again these two will be passed when we call fun
+*/
